2188ce59fc512a60340d9e11c8096f50a137723e
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-01-30 21:25:59 -0800

    playing with flexbox

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Another View Text Content</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your Course Goal!" />
+        <Button title="Add Goal" />
       </View>
-      <Text style={styles.dummyText}>Hello World!</Text>
-      <Button title="Tap Me!" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "springgreen",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: "blue",
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: "#CCCCCC",
   },
 });
